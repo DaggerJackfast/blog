37602493da4d5f408c0315d34a2a3cc025c6c77a
Aibek <aibek.abdykasymov@gmail.com>
2017-01-15 23:25:04 +0600

    Reorganized settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -1,5 +1,10 @@
-try:
-    from blog.setting_files.development import *
-except ImportError:
+import os
+SERVER = os.getenv('SERVER')
+if SERVER == 'production':
     from blog.setting_files.production import *
+else:
+    try:
+        from blog.setting_files.development import *
+    except ImportError:
+        from blog.setting_files.production import *
 
