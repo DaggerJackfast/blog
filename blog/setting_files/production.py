8a75d95fa48ecfded3b84268a6e9cf92c74c71ce
Aibek <aibek.abdykasymov@gmail.com>
2017-01-15 23:03:08 +0600

    Add test production settings

--- blog/setting_files/production.py
+++ blog/setting_files/production.py
@@ -2,7 +2,10 @@ import os
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = []
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
 
 MANAGERS = (('admin', 'jackfast.dagger@yandex.ru'))
 EMAIL_HOST = 'smtp.yandex.ru'
@@ -12,18 +15,36 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'jackfast.dagger@yandex.ru'
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog',
         'USER': 'djangouser',
         'PASSWORD': 'admin123',
         'PORT': '',
-        'OPTIONS': {
-            'init_command': 'SET default_storage_engine=INNODB',
-        }
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/collectstatic/')
